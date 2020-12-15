7608ebc2e682ef32ccbc11be8605e3f0ecdc6213
Igor360 <igor.dawidencko@gmail.com>
2020-12-15 03:56:15 +0200

    Some improvements

--- reducer.py
+++ reducer.py
@@ -5,10 +5,14 @@ import operator
 
 print("Month | \t Medium temperature")
 
+lines = sys.stdin
+next(lines)
+
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
+    if len(data_mapped) != 3:
         # Something has gone wrong
         continue
     thisMonth, thisSum, thisDays = data_mapped
-    print("{0}\tz{1}".format(thisMonth, thisSum / thisDays))
+    mediumTemp = float(thisSum) / int(thisDays)
+    print("{0}\t{1}".format(thisMonth, mediumTemp))
